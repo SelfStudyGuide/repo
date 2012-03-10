fa4b54a73e3956121b24eed8e020064417a7e3fc
maksymb <maksymb@daaeb461-b3eb-4b42-8a8b-4cd59dc14bb9>
2012-03-10 05:47:50 +0000

    Jog4j
    git-svn-id: https://bma.unfuddle.com/svn/bma_ssg/trunk@9 daaeb461-b3eb-4b42-8a8b-4cd59dc14bb9

--- ssg-gwt/src/main/java/org/ssg/gui/server/service/StudentControlServiceImpl.java
+++ ssg-gwt/src/main/java/org/ssg/gui/server/service/StudentControlServiceImpl.java
@@ -1,5 +1,7 @@
 package org.ssg.gui.server.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.ssg.gui.client.service.SsgGuiServiceException;
 import org.ssg.gui.client.service.StudentControlService;
@@ -10,12 +12,15 @@ import org.ssg.gui.shared.Response;
 public class StudentControlServiceImpl extends SpringGwtServlet implements
 		StudentControlService {
 
+	private static final Log LOG = LogFactory.getLog(StudentControlServiceImpl.class);
+	
 	private static final long serialVersionUID = 992653253296938793L;
 
 	@Autowired
 	private StudentUIService studentUIService;
 
 	public <T extends Response> T execute(Action<T> action) throws SsgGuiServiceException {
+		LOG.debug("executing " + action);
 		return studentUIService.execute(action);
 	}
 

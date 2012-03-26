03cdd7e3f5c66a5ed3b02afbcb72349591ecdb0c
maksymb <maksymb@daaeb461-b3eb-4b42-8a8b-4cd59dc14bb9>
2012-03-26 19:54:21 +0000

    Exception handling - ErrorDialog
    git-svn-id: https://bma.unfuddle.com/svn/bma_ssg/trunk@12 daaeb461-b3eb-4b42-8a8b-4cd59dc14bb9

---
+++ ssg-gwt/src/test/java/org/ssg/gui/client/errordialog/ErrorDialogTest.java
@@ -0,0 +1,92 @@
+package org.ssg.gui.client.errordialog;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.ssg.core.support.TstDataUtils;
+import org.ssg.core.support.TstDataUtils.TestAction;
+import org.ssg.gui.client.AbstractPresenterTestCase;
+import org.ssg.gui.client.errordialog.ErrorDialog.Display;
+
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.HasHTML;
+import com.google.gwt.user.client.ui.HasText;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ErrorDialogTest extends AbstractPresenterTestCase {
+	private ErrorDialog dialog;
+
+	@Mock
+	private Display view;
+
+	@Mock
+	private HasText actionInfoBox;
+
+	@Mock
+	private HasHTML messageBox;
+
+	@Mock
+	private HasClickHandlers closeBtn;
+
+	@Mock
+	private ErrorDialogMessages messages;
+
+	@Before
+	public void setUp() {
+		when(view.getActionInfoBox()).thenReturn(actionInfoBox);
+		when(view.getMessageBox()).thenReturn(messageBox);
+		when(view.getCloseButton()).thenReturn(closeBtn);
+		dialog = new ErrorDialog(view, messages);
+	}
+
+	@Test
+	public void verifyThatGwtDialogIsShowedWhenDialogIsCalled() {
+		TestAction action = new TstDataUtils.TestAction();
+		action.setActionName("Test/TestAction");
+
+		dialog.show("Error message", action);
+
+		verify(view).show();
+	}
+
+	@Test
+	public void verifyThatActionInfoIsUpdatedWhenDialogIsCalled() {
+		TestAction action = new TstDataUtils.TestAction();
+		action.setActionName("Test/TestAction");
+		when(messages.getActionInfo(eq("Test/TestAction"))).thenReturn(
+				"Action Name: Test/TestAction");
+
+		dialog.show("Error message", action);
+
+		verify(actionInfoBox).setText(eq("Action Name: Test/TestAction"));
+	}
+
+	@Test
+	public void verifyThatMessageInfoIsUpdatedWhenDialogIsCalled() {
+		TestAction action = new TstDataUtils.TestAction();
+		action.setActionName("Test/TestAction");
+		when(messages.getActionInfo(eq("Test/TestAction"))).thenReturn(
+				"Action Name: Test/TestAction");
+
+		dialog.show("Error message", action);
+
+		verify(messageBox).setHTML(eq("Error message"));
+	}
+	
+	@Test
+	public void verifyThatWindowIsClosedWhenClickCloseButton() {
+		ClickHandler handler = verifyAndCaptureClickHnd(closeBtn);
+		handler.onClick(null);
+
+		verify(view).hide();
+	}
+}

f1926fb9b82f2607434ae69aa8337c5d7ae11295
maksymb <maksymb@daaeb461-b3eb-4b42-8a8b-4cd59dc14bb9>
2012-05-20 21:44:45 +0000

    additional testing data
    git-svn-id: https://bma.unfuddle.com/svn/bma_ssg/trunk@22 daaeb461-b3eb-4b42-8a8b-4cd59dc14bb9

--- ssg-database/schema/9.init_data.sql
+++ ssg-database/schema/9.init_data.sql
@@ -9,5 +9,11 @@ insert into TOPIC_PROGRESS(STATUS, TOPIC_ID, HW_ID) values('0', 1, 3)
 insert into TOPIC_PROGRESS(STATUS, TOPIC_ID, HW_ID) values('10', 2, 3)
 insert into TOPIC_PROGRESS(STATUS, TOPIC_ID, HW_ID) values('100', 3, 3)
 
+insert into MODULE(id,desc, name) values(3, 'It is the sedond module', '2nd module');
+insert into HOMEWORK(id, stud_id) values(4, 1);
+insert into HOMEWORK_MODULE(HW_ID, MODULE_ID) values(3, 3);
+insert into TOPIC(id, name, mod_id) values(4, 'The 4rd topic', 3);
+insert into TOPIC_PROGRESS(STATUS, TOPIC_ID, HW_ID) values('100', 4, 4)
+
 insert into users values('jd','1',TRUE)
 insert into authorities values('jd','student');

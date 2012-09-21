4c96f2a681b8cc6466c645ffca86212b398a38c2
maksymb <maksymb@daaeb461-b3eb-4b42-8a8b-4cd59dc14bb9>
2012-09-21 19:23:17 +0000

    functional start-up test
    git-svn-id: https://bma.unfuddle.com/svn/bma_ssg/trunk@67 daaeb461-b3eb-4b42-8a8b-4cd59dc14bb9

---
+++ ssg-functional-test/src/test/java/org/ssg/test/functional/SimpleFunctionalTest.java
@@ -0,0 +1,60 @@
+package org.ssg.test.functional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.LifecycleState;
+import org.apache.catalina.startup.Tomcat;
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleFunctionalTest {
+
+	private Tomcat mTomcat;
+
+	private String mWorkingDir = System.getProperty("java.io.tmpdir") + "/" + getApplicationId();
+
+	@Test
+	public void test() throws LifecycleException {
+		assertThat(mTomcat.getServer().getState(), is(LifecycleState.STARTED));
+		
+	}
+
+	@Before
+	public void before() throws LifecycleException {
+	    mTomcat = new Tomcat();
+		mTomcat.setPort(0);
+		mTomcat.setBaseDir(mWorkingDir);
+		mTomcat.getHost().setAppBase(mWorkingDir);
+		mTomcat.getHost().setAutoDeploy(true);
+		mTomcat.getHost().setDeployOnStartup(true);
+
+		String contextPath = "/" + getApplicationId();
+		File webApp = new File("target", "ssg-gui.war");
+
+		mTomcat.addWebapp(mTomcat.getHost(), contextPath, webApp.getAbsolutePath());
+		
+		mTomcat.start();
+    }
+
+	@After
+	public final void teardown() throws Throwable {
+	  if (mTomcat.getServer() != null
+	            && mTomcat.getServer().getState() != LifecycleState.DESTROYED) {
+	        if (mTomcat.getServer().getState() != LifecycleState.STOPPED) {
+	              mTomcat.stop();
+	        }
+	        mTomcat.destroy();
+	    }
+	}
+	
+	private String getApplicationId() {
+		return "ssg-gui-functional";
+	}
+}
